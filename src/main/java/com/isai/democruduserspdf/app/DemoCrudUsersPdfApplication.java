c35dbece9133b7c8adfafeaf86ad575e5a23670c
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-09 00:21:37 -0500

    init

---
+++ src/main/java/com/isai/democruduserspdf/app/DemoCrudUsersPdfApplication.java
@@ -0,0 +1,13 @@
+package com.isai.democruduserspdf.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrudUsersPdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCrudUsersPdfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isai/democruduserspdf/app/DemoCrudUsersPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.democruduserspdf.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrudUsersPdfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
