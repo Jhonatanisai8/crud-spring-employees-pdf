4a7fd3358f2d294f1f554964fbe78d2fc0e932e6
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-10 12:14:00 -0500

    ultimas configuraciones para el login

---
+++ src/main/java/com/isai/democruduserspdf/app/config/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.isai.democruduserspdf.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("jhonatan")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("eli")
+                .password(passwordEncoder().encode("password"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        String[] allowedPaths = {"/register-employee", "/edit-employee/**", "/remove-employee/**"};
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/").permitAll()
+                .requestMatchers(allowedPaths).hasRole("ADMIN")
+                .anyRequest().authenticated()
+        ).formLogin(formLogin -> formLogin
+                .loginPage("/login")
+                .permitAll()
+        ).logout(LogoutConfigurer::permitAll
+        );
+        return http.build();
+    }
+
+}
