35d036156582506b281078803cb14507a6adee8b
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-09 23:24:54 -0500

    Metodo para mostrar el formulario donde se pondran los datos del empleado ademas del metodo guardar

--- src/main/java/com/isai/democruduserspdf/app/controller/EmployeeController.java
+++ src/main/java/com/isai/democruduserspdf/app/controller/EmployeeController.java
@@ -3,13 +3,16 @@ package com.isai.democruduserspdf.app.controller;
 import com.isai.democruduserspdf.app.models.Employee;
 import com.isai.democruduserspdf.app.repositorys.EmployeeRepository;
 import com.isai.democruduserspdf.app.utils.pagination.PageRender;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Map;
@@ -32,12 +35,12 @@ public class EmployeeController {
         return "list-employees";
     }
 
-    @GetMapping("/verDetalleEmpleado/{employeeId}")
+    @GetMapping(path = "/verDetalleEmpleado/{employeeId}")
     public String viewEmployeeDetail(
             @PathVariable(value = "employeeId") Long employeeId,
             Map<String, Object> model,
             RedirectAttributes flash) {
-        Employee employee = employeeRepository.findById(id).orElseThrow();
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow();
         if (Objects.isNull(employee)) {
             flash.addFlashAttribute("message", "El empleado no existe.");
             return "redirect:/list";
@@ -46,4 +49,32 @@ public class EmployeeController {
         model.put("tittle", "Detalle del empleado.".concat(employee.getFirstName()));
         return "view-employee-details";
     }
+
+    @GetMapping(path = "/register-employee")
+    public String mostrarFormularioRegistrarEmpleado(
+            Map<String, Object> model) {
+        Employee employee = new Employee();
+        model.put("tittle", "Registrar Empleado");
+        model.put("employee", employee);
+        return "form-employee";
+    }
+
+    @PostMapping(path = "/register-employee")
+    public String guardarEmpleado(
+            @Valid Employee employee,
+            BindingResult result,
+            Model model,
+            RedirectAttributes flash,
+            SessionStatus status) {
+        if (result.hasErrors()) {
+            model.addAttribute("tittle", "Registrar Empleado");
+            return "form-employee";
+        }
+        String message = (employee.getIdEmployee() != null) ? "Empleado actualizado exitosamente." : "Empleado registrado exitosamente.";
+        employeeRepository.save(employee);
+        status.setComplete();
+        flash.addFlashAttribute("message", message);
+        return "redirect:/list";
+    }
+
 }
